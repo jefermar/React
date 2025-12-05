7f90e0b004f4060e584901496317aeb1f6216e9d
jefermar <jefermar17jjs@gmail.com>
2025-12-05 16:49:12 -0500

    create_componets

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const cursos = ["React", "HTML", "JavaScript"]
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section>
+      <h1>Hola Mundo desde React!</h1>
+      <Card title={"curso react"} description={"en este curso aprenderas un nuevo framework"} />
+      <Card title={"curso de bootstrap"} description={"en este curso aprenderas como desarrollar mejor fronted"} />
+      <ul>
+        {cursos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </section>
   )
 }
 
